6a7ae52874433cd59042f61a42c7e87eabbd31e3
tlegutko <tom.legutko@gmail.com>
2017-06-24 02:00:40 +0200

    Finished laziness chapter.

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -99,9 +99,9 @@ trait Stream[+A] {
 
   def takeWhileWithFold(p: A => Boolean): Stream[A] = {
     foldRight(empty: Stream[A])((a, b) => p(a) match {
-                       case true => cons(a, b)
-                       case false => empty
-                     })
+      case true => cons(a, b)
+      case false => empty
+    })
   }
 
   def headOption: Option[A] = this match {
@@ -118,13 +118,66 @@ trait Stream[+A] {
   def filter(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((a, b) => if (f(a)) cons(a, b) else b)
 
-  def append[B>:A](x: => Stream[B]): Stream[B] =
+  def append[B >: A](x: => Stream[B]): Stream[B] =
     foldRight(x)((a, b) => cons(a, b))
 
   def flatMap[B](f: A => Stream[B]): Stream[B] =
     foldRight(empty[B])((a, b) => f(a).append(b))
 
-  def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+  def mapWithUnfold[B](f: A => B): Stream[B] =
+    unfold(this) {
+      case Cons(h, t) => Some((f(h()), t()))
+      case _ => None
+    }
+
+  def takeWithUnfold(n: Int): Stream[A] =
+    unfold((this, n)) {
+      case (Cons(h, t), x) if x > 0 => Some((h(), (t(), x - 1)))
+      case _ => None
+    }
+
+  def takeWhileWithUnfold(p: A => Boolean): Stream[A] =
+    unfold(this) {
+      case Cons(h, t) if p(h()) => Some((h(), t()))
+      case _ => None
+    }
+
+  def zip[B](s2: Stream[B]): Stream[(A, B)] =
+    unfold((this, s2)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(((h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
+    unfold((this, s2)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2())), (t1(), t2())))
+      case (Cons(h1, t1), empty) => Some(((Some(h1()), None), (t1(), empty)))
+      case (empty, Cons(h2, t2)) => Some(((None, Some(h2())), (empty, t2())))
+      case _ => None
+    }
+
+  def startsWith[B](s: Stream[B]): Boolean = {
+    zip(s).forAll {
+      case (a, b) if a == b => true
+      case _ => false
+    }
+  }
+
+  def tails(): Stream[Stream[A]] =
+    unfold(this) {
+      case s @ Cons(h, t) => Some((s, t()))
+      case _ => None
+    } append Stream(empty)
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = {
+    foldRight(z, Stream(z)){
+      case (h, (x, s)) => {
+        val fhx = f(h, x)
+        (fhx, cons(fhx, s))
+      }
+    }._2
+  }
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -151,7 +204,7 @@ object Stream {
     cons(a, constant(a))
 
   def from(n: Int): Stream[Int] =
-    cons(n, from(n+1))
+    cons(n, from(n + 1))
 
   def fibs(): Stream[Int] = {
     def go(curr: Int, prev: Int): Stream[Int] = {
@@ -178,5 +231,6 @@ object Stream {
 >>>>>>> Unfold (ex. 10-11 from laziness).
 
   def fibsWithUnfold(): Stream[Int] =
-    unfold(1, 0){ case (curr, prev) => Some((prev, (curr + prev, curr))) }
+    unfold(1, 0) { case (curr, prev) => Some((prev, (curr + prev, curr))) }
+
 }
--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -38,21 +38,43 @@ class StreamSpec extends FunSuite {
   }
 
   test("constant, from, fibs") {
-    assert(Stream.constant(("a", 3)).take(2).toList() == List(("a", 3), ("a", 3))) 
+    assert(Stream.constant(("a", 3)).take(2).toList() == List(("a", 3), ("a", 3)))
     assert(Stream.from(10).take(4).toList() == List(10, 11, 12, 13))
     assert(Stream.fibs().take(7).toList == List(0, 1, 1, 2, 3, 5, 8))
   }
 
   test("unfold") {
     assert(Stream.unfold(List(1, 2, 3))(l => l match {
-                                         case h :: t => Some((h * 3, t))
-                                         case Nil => None
-                                       }).toList() == List(3, 6, 9))
+      case h :: t => Some((h * 3, t))
+      case Nil => None
+    }).toList() == List(3, 6, 9))
   }
 
   test("constant, from, fibs using unfold") {
     assert(Stream.constantWithUnfold(("a", 3)).take(2).toList() == List(("a", 3), ("a", 3)))
     assert(Stream.fromWithUnfold(10).take(4).toList() == List(10, 11, 12, 13))
     assert(Stream.fibsWithUnfold().take(7).toList == List(0, 1, 1, 2, 3, 5, 8))
+  }
+
+  test("map, take, takeWhile, zip, zipAll with unfold") {
+    val s = Stream(1, 2, 3, 4)
+    assert(s.takeWithUnfold(3).toList() == List(1, 2, 3))
+    assert(s.takeWhileWithUnfold(_ <= 3).toList() == List(1, 2, 3))
+    assert(s.mapWithUnfold(_ * 2).toList() == List(2, 4, 6, 8))
+    assert(s.zip(Stream("a", "b", "c", "d")).toList() == List((1, "a"), (2, "b"), (3, "c"), (4, "d")))
+    assert(s.zipAll(Stream("a", "b")).toList() == List((Some(1), Some("a")), (Some(2), Some("b")), (Some(3), None), (Some(4), None)))
+  }
+
+  test("startsWith") {
+    assert(Stream(1, 2, 3).startsWith(Stream(1, 2)) == true)
+    assert(Stream(1, 2, 3).startsWith(Stream(1, 3)) == false)
+  }
+
+  test("tails") {
+    assert(Stream(1, 2, 3).tails().toList().map(_.toList()) == List(List(1, 2, 3), List(2, 3), List(3), List.empty))
+  }
+
+  test("scanRight") {
+    assert(Stream(1, 2, 3).scanRight(0)(_ + _).toList() == List(6, 5, 3, 0))
   }
 }
